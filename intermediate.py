373477b02271a9379f3bebeab595346e6b300611
Gloriel-Soto <gloriel.soto@upr.edu>
2021-11-30 10:57:01 -0400

    Added Intermediate Code

--- intermediate.py
+++ intermediate.py
@@ -1,107 +1,204 @@
-import string
-
-import lexer
-import syntax
-from tabulate import tabulate
-
-# def createvm(name: lexer.t_NAME, region: lexer.t_REGION, server: lexer.t_SERVER, os: lexer.t_OS):
-# name = input('Please enter the Name')
+#!/usr/bin/python
+###########################################################################
 #
-# if name != lexer.t_NAME:
-# print('Please enter a valid Name. Run again.')
-#   exit()
+# ADD YOUR DigitalOcean TOKEN BELOW
+# ---------------------------------
+token = ""
 #
-# d = [["NYC1, NYC2*, NYC3", "New York City, United States"],
-#     ["AMS2*, AMS3", "Amsterdam, the Netherlands"],
-#     ["SFO1*, SFO2*", "San Francisco, United States"],
-#    ["SGP1", "Singapore"],
-#    ["LON1", "London, United Kingdom"],
-#      ["FRA1", "Frankfurt, Germany"],
-#         ["TOR1", "Toronto, Canada"],
-#         ["BLR1", "Bangalore, India"]]
-#
-# print("\n" + tabulate(d, headers=['Name', 'Region']))
-#
-# region = input('\n' + 'Please enter the Name of the Region you want.')
-#
-#   if region != lexer.t_REGION and tabulate('Name'):
-#        print('Please enter a valid Name. Run again.')
-#   exit()
-#
-#   server = input('Please enter the Server')
-#
-#   if server != lexer.t_SERVER:
-#        print('Please enter a valid Server. Run again.')
-#   exit()
-#
-# d = [["69452245", "freebsd-11-x64-zfs", "FreeBSD 11.4 ZFS", "free11z", "z11"],
-#    ["69500386", "freebsd-11-x64-ufs", "FreeBSD 11.4 UFS", "free11u", "u11"],
-#    ["77558491", "freebsd-12-x64-ufs", "FreeBSD 12.2 UFS", "free12u", "u12"],
-#    ["77558552", "freebsd-12-x64-zfs", "FreeBSD 12.2 ZFS", "free12z", "z12"],
-#    ["78547182", "rancheros", "RancherOS 1.5.8 x86 image", "ranch1", "r1"],
-#    ["84780898", "fedora-34-x64", "Fedora 34 x64", "fedora34", "f34"],
-#    ["85722003", "centos-7-x64", "CentOS 7 x64", "centos7", "c7"],
-#    ["85779954", "centos-8-x64", "CentOS 8.3 x64", "centos8", "c8"],
-#    ["85779974", "debian-9-x64", "Debian 9 x86 image", "debian9", "d9"],
-#    ["85780245", "fedora-33-x64", "Fedora 33 x64", "fedora33", "f33"],
-#    ["86718194", "debian-10-x64", "Debian 10 Image", "debian10", "d10"],
-#    ["89199942", "rockylinux-8-x64", "RockyLinux 8.4 x64", "rocky8", "r8"],
-#    ["89246461", "centos-stream-8-x64", "CentOS Stream 8 x64", "centos8s", "s8"],
-#    ["92517214", "debian-11-x64", "Debian 11 Image", "debian11", "d11"],
-#    ["93524084", "ubuntu-18-04-x64", "Ubuntu 18.04 x86 image", "ubuntu18", "u18"],
-#       ["93525508", "ubuntu-20-04-x64", "Ubuntu 20.04 x86", "ubuntu20", "u20"],
-#       ["94389391", "ubuntu-21-10-x64", "Ubuntu 21.10 x64", "ubuntu21", "u21"],
-#        ["95344509", "fedora-35-x64", "Fedora 35 x64", "fedora35", "f35"]]
-#
-#   print("\n" + tabulate(d, headers=['id', 'Slug', 'Description', 'option2', 'Unique Name']))
-#
-#   os = input("\n" + 'Please enter the unique name of the Operating System. The Operating Systems are: ')
-#
-#  if os != lexer.t_OS and tabulate('Name'):
-#       print('Please enter a valid Name.Run again.')
-#        exit()
+###########################################################################
 
-#Created 11/24/21
+import sys
+import time
+import digitalocean
+import xml.etree.ElementTree as ET
 
-import lang
+version = "1.0"
+c = {"r": "\033[1;31m", "g": "\033[1;32m", "y": "\033[1;33m", "e": "\033[0m"}
+manager = digitalocean.Manager(token=token)
 
 
-def CREATE(name, region, server, os):
-    name = lang.LETTERS
-    region = lang.LETTERS
-    server = lang.LETTERS
-    os = lang.LETTERS_DIGITS
+def get_droplets():
+    return manager.get_all_droplets()
 
 
-def DELETE(iden, tag):
-    iden = lang.LETTERS_DIGITS
-    tag = lang.LETTERS
+def get_my_token(token):
+    return getpass.getpass()
 
 
-def SHOW(iden, tag):
-    iden = lang.LETTERS_DIGITS
-    tag = lang.LETTERS
+def create_droplet(token, name, region, os, ram, ssh):
+    if len(ssh) < 1:
+        ssh = ""
+
+    droplet = digitalocean.Droplet(token=token,
+                                   name=name,
+                                   region=region,
+                                   image=os,
+                                   size_slug=ram,
+                                   backups=False,
+                                   ssh_keys=ssh)
+
+    x = droplet.create()
+
+    print("%s[Droplet]%s '%s' Created." % (c["g"], c["e"], name))
 
 
-if __name__ == '__main__':
+def interactive_build():
+    num = int(input("How many droplets do you want to create: "))
 
-    text = input("Please enter your command (Create, Delete, Show) or write stop to end program ")
+    name = input("VPS name (if # of VPS > 1, name will become %name%-01, %name%-02 etc): ")
 
-    if text == 'stop':
+    for i in manager.get_all_regions():
+        print("%s : '%s'" % (i.name.strip(), i.slug.strip()))
+    region = input("What region: ")
+
+    for i in manager.get_all_images():
+        print("'%s'" % (i.slug))
+    os = input("What base image: ")
+
+    ram = input("What size RAM (e.g. 4096): ")
+    if "mb" in ram or "MB" in ram:
+        ram = ram[:2]
+
+    for n in xrange(num - 1):
+        x = create_droplet(token, name, region, os, ram)
+        print("\033[1;32m[+]\033[0m Droplet '%s' created -> IP address '%s'" % (str(name + str("-" + n)), x.ip_address))
+
+
+def parse_file(file):
+    try:
+        f = open(file, "r").close()
+    except:
+        print("%s[Error]%s Accessing file '%s'" % (c["r"], c["e"], file))
         exit()
-    elif text == 'create':
-        CREATE()
-    elif text == 'delete':
-        DELETE()
-    elif text == 'show':
-        SHOW()
 
-    result, error = lang.run('<stdin>', text)
+    d = {"amount": 1, "name": "newDroplet", "ram": "512", "location": "lon1", "image": "fedora-22-x64", "ssh_key": ""}
 
-#Network1 = {type : private, ip : 4, region : New York}
-#Droplets1 = {PremiumIntel1 : RockyLinux8.4x64 , Basic2 : 
-#CentOS8x64, PremiumAMD1 : FreeBSD12.2}
-#for i in droplets1:
-#Create droplets1(i) in network1
-#Status = status + “/n“ + (return)
-#Print (status)
+    try:
+        root = ET.parse(file).getroot()
+    except:
+        print("%s[Error]%s Invalid XML file '%s', refer to README for schema information." % (c["r"], c["e"], file))
+        exit()
+
+    for child in root:
+        childd = child.tag.lower()
+        if childd == "amount":
+            d["amount"] = int(child.text)
+
+        elif childd == "name":
+            d["name"] = child.text.lower()
+
+        elif (childd == "ram") and (int(child.text) >= 256):
+            d["ram"] = int(child.text)
+
+        elif childd == "location":
+            d["location"] = child.text.lower()
+
+        elif childd == "image":
+            d["image"] = child.text.lower()
+
+        elif childd == "ssh_key":
+            d["ssh_key"] = []
+            d["ssh_key"].append(child.text.lower())
+        else:
+            print("%s[Error]%s Invalid XML Schema, tag '%s' is not a valid identifier, see README." % (
+                c["r"], c["e"], childd))
+            exit()
+
+    return d
+
+
+def create_droplets_from_list(drop_dic):
+    for num in range(0, drop_dic["amount"]):
+        name = drop_dic["name"] + "-" + str(num)
+        ram = str(drop_dic["ram"]) + "mb"
+        img = drop_dic["image"]
+        loc = drop_dic["location"]
+        ssh = drop_dic["ssh_key"]
+        create_droplet(token, name, loc, img, ram, ssh)
+
+
+def usage():
+    print("\n# python %s [options]" % sys.argv[0])
+    print("")
+    print("Options: ")
+    print("\t-l\t[arg]\t\t-\tList information about '[arg]' -> droplets, loc, img")
+    print("\t-f\t[file.xml]\t-\tCreate droplets from XML schema, see README.")
+    print("\t-i\t\t\t-\tCreate droplets from interactive menu (Easy)")
+    print("\t-rm\t[id]\t\t-\tDelete droplet [id]")
+    print("")
+    exit(1)
+
+
+if __name__ == "__main__":
+
+    if token == "":
+        print("%s[Error]%s Please first set your DigitalOcean API token at the top of the script." % (c["r"], c["e"]))
+        exit()
+
+    if "-rm" in sys.argv:
+
+        id = int(sys.argv[sys.argv.index("-rm") + 1])
+
+        d = get_droplets()
+
+        try:
+            name = d[id].name
+        except:
+            print(
+                "%s[Error]%s Droplet ID out of range, do 'python %s -l droplets' to see how many droplets you have." % (
+                    c["r"], c["e"], sys.argv[0]))
+            exit()
+
+        ch = input("%s[Confirm]%s Are you sure you want to delete droplet '%s' [Y/N]: " % (c["r"], c["e"], name))
+
+        if "y" in ch or "Y" in ch:
+
+            try:
+                d[id].destroy()
+            except:
+                print("%s[Error]%s DataReadError from DigitalOcean, retry again later" % (c["r"], c["e"]))
+                exit(1)
+        print("%s[Done]%s Droplet '%s' destroyed." % (c["g"], c["e"], name))
+        exit()
+
+    elif "-l" in sys.argv:
+        option = sys.argv[sys.argv.index("-l") + 1]
+
+        if option.lower() == "img":
+            print("%s[Option]%s All valid base images." % (c["y"], c["e"]))
+            im = manager.get_distro_images()
+            for i in im:
+                print("%s[Image]%s %s -> '%s'" % (c["g"], c["e"], str(i.distribution + " " + i.name), i.slug))
+            exit()
+
+        elif option.lower() == "loc":
+            print("%s[Option]%s All regions." % (c["y"], c["e"]))
+            r = manager.get_all_regions()
+            for i in r:
+                print("%s[Region]%s %s -> '%s'" % (c["g"], c["e"], i.name, i.slug))
+            exit()
+
+        elif option.lower() == "droplets":
+            print("%s[Option]%s All your Droplets." % (c["y"], c["e"]))
+            d = get_droplets()
+            count = 0
+            for i in d:
+                print("%s[Droplet id: '%d']%s '%s' -> '%s'" % (c["g"], count, c["e"], i.name, i.ip_address))
+                count += 1
+            exit()
+
+        else:
+            print("%s[Error]%s Option '%s' not recognised, see '%s --help'" % (c["r"], c["e"], option, sys.argv[0]))
+            exit()
+
+    elif "-i" in sys.argv:
+        interactive_build()
+        exit()
+
+    elif "-f" in sys.argv:
+        list = parse_file(sys.argv[sys.argv.index("-f") + 1])
+        create_droplets_from_list(list)
+        exit()
+
+    else:
+        usage()
