5230d2b4f2e68360e050775faa76fa5d47cce253
Haniel Cordero <43764739+hancord@users.noreply.github.com>
2021-12-01 17:39:15 -0400

    Update intermediate.py

--- intermediate.py
+++ intermediate.py
@@ -5,6 +5,7 @@ import sys
 import time
 import digitalocean
 import xml.etree.ElementTree as ET
+import json
 
 ###########################################################################
 #
@@ -15,32 +16,30 @@ version = "1.0"
 c = {"r": "\033[1;31m", "g": "\033[1;32m", "y": "\033[1;33m", "e": "\033[0m"}
 manager = digitalocean.Manager(token=token)
 
+ram = "s-1vcpu-1gb"
 
 def get_droplets():
     return manager.get_all_droplets()
 
 
-def get_token(token):
-    return getpass.getpass()
+def get_token(token):    return getpass.getpass()
 
 
 def create(token, name, region, os, ram):
-
     droplet = digitalocean.Droplet(token=token,
                                    name=name,
                                    region=region,
                                    image=os,
                                    size_slug=ram,
                                    backups=False)
-
-    x = droplet.create()
-
+    droplet.create()
     print("%s[Droplet]%s '%s' Created." % (c["g"], c["e"], name))
 
 
 def build():
-    num = int(input("How many droplets do you want to create: "))
-
+#    print("build initial")
+#    num = int(input("How many droplets do you want to create: "))
+    num = 1
     name = input("VPS name (if # of VPS > 1, name will become %name%-01, %name%-02 etc): ")
 
     for i in manager.get_all_regions():
@@ -51,28 +50,69 @@ def build():
         print("'%s'" % i.slug)
     os = input("What base image (i.e. rockylinux-8-x64): ")
 
-    ram = "512mb"
+    ram = "s-1vcpu-1gb"
 
-    for n in range(num - 1):
-        cdp = create(token, name, region, os, ram)
-        print("\033[1;32m[+]\033[0m Droplet '%s' created -> IP address '%s'" % (str(name + str("-" + n)), cdp.ip_address))
+    print(num, name, region, os, ram)
+#    for n in range(num - 1):
+    droplet = digitalocean.Droplet(token=token,
+                                   name=name,
+                                   region=region,
+                                   image=os,
+                                   size_slug=ram,
+                                   backups=False)
+    response = droplet.create()
+    print("Creating a ", ram, "'s VM named ", name, " in ", region, " with ", os, ". Please, wait a minute until creation.")
+    time.sleep(5)
+    print("[.                                                              ]")
+    time.sleep(5)
+    print("[...                                                            ]")
+    time.sleep(5)
+    print("[.....                                                          ]")
+    time.sleep(5)
+    print("[.......                                                        ]")
+    time.sleep(5)
+    print("[..........                                                     ]")
+    time.sleep(5)
+    print("[...............                                                ]")
+    time.sleep(5)
+    print("[...................                                            ]")
+    time.sleep(5)
+    print("[............................                                   ]")
+    time.sleep(5)
+    print("[........................................                       ]")
+    time.sleep(5)
+    print("[..................................................             ]")
+    time.sleep(5)
+    print("[............................COMPLETED..........................]")
+    time.sleep(5)
 
+#    print("\033[1;32m[+]\033[0m Droplet '%s' created -> IP address '%s'" % (str(name + str("-" + n)), response.ip_address))
+    print(name,"'s VM has been created.")
+    print(get_droplets())
 
 def create_from_list(drop_dic):
     for num in range(0, drop_dic["amount"]):
         name = drop_dic["name"] + "-" + str(num)
-        ram = "512mb"
+        ram = "s-1vcpu-1gb"
         img = drop_dic["image"]
-        loc = drop_dic["location"]
-        create(token, name, loc, img, ram)
+        region = drop_dic["region"]
+#        create(token, name, region, img, ram)
 
+        droplet = digitalocean.Droplet(token=token,
+                                       name=name,
+                                       region=region,
+                                       image=img,
+                                       size_slug=ram,
+                                       backups=False)
+
+        droplet.create()
 
 def usage():
     print("")
     print("# Run in CMD as: %s [options]" % sys.argv[0])
     print("")
     print("Options: ")
-    print("\t-l\t[arg]\t\t-\tList information about '[arg]' -> droplets, loc, img")
+    print("\t-l\t[arg]\t\t-\tList information about '[arg]' -> droplets, region, img")
     print("\t-i\t\t\t\t-\tCreate droplets from interactive menu (Easy)")
     print("\t-rm\t[id]\t\t-\tDelete droplet [id]")
     print("")
